cdac1cf1713deba4b5a874aa80f8576f0b7c9469
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-10-19 14:11:23 -0400

    forEach() and IIFE
    JS code for Pokedex page was updated to include a forEach() loop. Also, the pokemonList array was placed inside an IIFE and the functions "add" and "getAll" were added to access and/or modify the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,54 @@
-// Define Pokemon List as an array
-let pokemonList = [];
-// Add pokemon to pokemonList array. Currently 5 pokemon (objects).
-pokemonList = [
-    {
-    name: "Venusaur",
-    height: 2,
-    type: ["Grass", "Poison"]
-    }, 
-    {
-    name: "Blastoise",
-    height: 1.6,
-    type: ["Water"]
-    }, 
-    {
-    name: "Charizard", 
-    height: 1.7, 
-    type: ["Fire", "Flying"]
-    }, 
-    {
-    name: "Pikachu", 
-    height: 0.4, 
-    type: ["Electric"]}, 
-    {
-    name: "Cubone", 
-    height: 0.4, 
-    type: ["Ground"]
+// IIFE created using "pokemonRepository" as its name.
+let pokemonRepository = (function(){
+// The pokemonList array has been defined here.
+    let pokemonList = [
+        {
+        name: "Venusaur",
+        height: 2,
+        type: ["Grass", "Poison"]
+        }, 
+        {
+        name: "Blastoise",
+        height: 1.6,
+        type: ["Water"]
+        }, 
+        {
+        name: "Charizard", 
+        height: 1.7, 
+        type: ["Fire", "Flying"]
+        }, 
+        {
+        name: "Pikachu", 
+        height: 0.4, 
+        type: ["Electric"]}, 
+        {
+        name: "Cubone", 
+        height: 0.4, 
+        type: ["Ground"]
+        }
+    ];
+// The following function will allow users to add pokemon to the pokemonList array.
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
-// Create a "for" loop that will list Pokemon from pokemonList with their corresponding heights.
-for (let i = 0; i < pokemonList.length; i++){
-      if (pokemonList[i].height > 0.0 && pokemonList[i].height <= 0.9){
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br>");
-    } else if (pokemonList[i].height >= 1.0 && pokemonList[i].height <= 1.9){
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br>");
+// The following function will allow users to call on and list the objects inide the pokemonList array.
+    function getAll() {
+        return pokemonList;
+    }
+// The above-mentioned functions have been further defined/labeled here to allow for easier access from outside the IIFE.
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+// A forEach() loop has been created to access the pokemonList inside the pokemonRepository
+pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height > 0.0 && pokemon.height <= 0.9){
+        document.write("<b>" + pokemon.name + "</b>" + " (height: " + pokemon.height + ") - <i>Small</i> <br>");
+    } else if (pokemon.height >= 1.0 && pokemon.height <= 1.9){
+        document.write("<b>" + pokemon.name + "</b>" + " (height: " + pokemon.height + ") - <i>Medium</i> <br>");
     } else {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - <i>Wow, that's big !</i> <br>");
+        document.write("<b>" + pokemon.name + "</b>" + " (height: " + pokemon.height + ") - <i>Wow, that's big !</i> <br>");
     }
-}
+});
