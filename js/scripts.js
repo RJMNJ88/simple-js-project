c381892200cf8e26b5b7aaabe47cb017ec9d099e
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-11-02 16:32:41 -0400

    Exercise 1.8 Trial

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,85 @@
 // IIFE created using "pokemonRepository" as its name.
 let pokemonRepository = (function(){
-// The pokemonList array has been defined here.
+
+    // The pokemonList array has been defined here.
     let pokemonList = [];
-// Create a link to access the pokemonAPI webpage
+
+    // Create a link to access the pokemonAPI webpage
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-// The following function will allow users to add pokemon to the pokemonList array.
+    // Create a variable for the modal
+    let modalContainer = document.querySelector('#modal-container');
+
+    function showModal(pokemon) {
+        // Clear all content from previously created <div>
+        modalContainer.innerHTML = "";
+        // Create a new <div> inside "modalContainer"
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        // Create the close button for the modal
+        // Add an event listener to the button to allow for closing out the modal
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        // Create the pokemon title, height, and type elements for the modal
+        let titleElement = document.createElement('h1');
+        titleElement.classList.add('title-element');
+        titleElement.innerText = item.name;
+        let heightElement = document.createElement('p')
+        heightElement.classList.add('height-element');
+        heightElement.innerText = 'Height: ' + pokemon.height;
+        let typeElement = document.createElement('p');
+        typeElement.classList.add('type-element');
+        typeElement.innerText = 'Type: ' + pokemon.type;
+        let imageElement = document.createElement('img');
+        imageElement.classList.add('image-element');
+        img.url = pokemon.sprites.other['official-artwork']['front_default'];
+
+        // Append all newly created elements
+        modal.appendChild('modal-close');
+        modal.appendChild('title-element');
+        modal.appendChild('height-element');
+        modal.appendChild('type-element');
+        modal.appendChild('image-element');
+        modalContainer.appendChild('modal');
+
+        // Create a class to enable toggling of the modal container's visibility
+        modalContainer.classList.add('is-visible');
+    }
+
+    // Create a function for hiding the modal container
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    }
+
+    // Add an Event Listener to hide the modal whenthe user presses the "Escape" key
+    window.addEventListener('keydown', (e) => {
+        if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+            hideModal();
+        }
+    });
+
+    // Add an Event Listener to hide the modal container when user clicks outside of the modal
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
+
+    // The following function will allow users to add pokemon to the pokemonList array.
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
-// The following function will allow users to call on and list the objects inide the pokemonList array.
+
+    // The following function will allow users to call on and list the objects inide the pokemonList array.
     function getAll() {
         return pokemonList;
     }
 
-// The following function will create a list of buttons representing each Pokemon in the Pokedex.
+    // The following function will create a list of buttons representing each Pokemon in the Pokedex.
     function addListItem(pokemon) {
         let pokeList = document.querySelector('.pokemon-list');
         let pokeListItem = document.createElement('li');
@@ -30,9 +95,7 @@ let pokemonRepository = (function(){
 
     }
 
-   
-
-// Create a function to GET information from the pokemonAPI webpage
+    // Create a function to GET information from the pokemonAPI webpage
     function loadList() {
         return fetch(apiUrl).then(function(response){
             return response.json();
@@ -50,7 +113,7 @@ let pokemonRepository = (function(){
         })
     }
 
-// Adding details to the fetch() from the pokemonAPI
+    // Adding details to the fetch() from the pokemonAPI
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function(response){
@@ -66,11 +129,11 @@ let pokemonRepository = (function(){
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function(){
-            console.log(pokemon);
+            showModal(pokemon);
         });
     }
 
-// The above-mentioned functions have been further defined/labeled here to allow for easier access from outside the IIFE.
+    // The above-mentioned functions have been further defined/labeled here to allow for easier access from outside the IIFE.
     return {
         add: add,
         getAll: getAll,
@@ -82,7 +145,6 @@ let pokemonRepository = (function(){
 })();
 
 // A forEach() loop has been created to access the pokemonList inside the pokemonRepository
-
 pokemonRepository.getAll().forEach(function (pokemon){
 
     pokemonRepository.addListItem(pokemon);
